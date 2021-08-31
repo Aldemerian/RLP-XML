09d9e969bdba43945b459c3fb28c1ce0ea359289
Aldemerian <maike21031991@googlemail.com>
2021-08-31 22:50:53 +0200

    Update xmlToJson.py

--- xmlToJson.py
+++ xmlToJson.py
@@ -3,26 +3,33 @@ import xml.etree.ElementTree as ET
 demo=input("Give me your Poor, your huddled XML-File_Path: ")
 
 f = open(demo, "r")
-print(f.read()) 
+#print(f.read()) 
+allSkills=[]
 
 tree = ET.parse(f)
 root = tree.getroot()
-for subarea in root.iter('subarea'):
-	print(subarea.attrib)
-	for sub in subarea.iter("competence"):
-		print(sub.attrib)
-		for comp in sub.iter("standard"):
-			print(comp.attrib)
+#print(tree.tag)
+for subarea in tree.findall('subarea'):
+	print(subarea)
+	print("ha")
+	for sub in subarea.findall("competence"):
+		print(sub)
+		print("ho")
+		for comp in sub.findall("standard"):
+			#print(comp.attrib)
+			skillId=1
+			print("hi")
 			for skill in comp:
-				skillId=skillId++
+				skillId+=skillId
+				print(skill)
 				outputSkill={"id": skillId,
-					     "name":skill.name.attrib,
+					     "name":skill.findall("name").attrib,
 					     "fach":"Mathe",
 					     "alter":"",
 					     "location": {
 						     "bundesland": "Berlin-Brandenburg"
 					     },
-					     "alternateName": [skill.id.attrib, skill.name.attrib, skill.name.content],
+					     "alternateName": [skill.findall("id").attrib, skill.name.attrib, skill.name.content],
 					     "category":"Logik",
 					     "subcategory":sub.name.attrib,
 					     "bloom": skill.stufe.level.attrib,
@@ -36,3 +43,5 @@ for subarea in root.iter('subarea'):
 				
 				
 print(allSkills)
+
+
